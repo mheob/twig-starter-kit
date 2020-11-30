bd1f2d6d38c781668de462c133376681c9db14b3
Comar <comar@100herz.de>
2020-11-30 14:10:32 +0100

    add local custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      display: ['Open Sans'],
+      body: ['Open Sans'],
+    },
     extend: {
       height: {
         '50vh': '50vh',
