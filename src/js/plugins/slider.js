4070ab4c800f4f62d0c18a9a6b674de8df3b49d3
Alexander Böhm <tools@boehm.work>
2020-12-06 15:08:56 +0100

    refactor(slider): remove the repetitions of function calls
    See ABC-Team as a more complex example.

--- src/js/plugins/slider.js
+++ src/js/plugins/slider.js
@@ -1,6 +1,6 @@
 import Flickity from 'flickity';
 
-const generellSliderSettings = {
+const generalSliderSettings = {
   arrowShape:
     // eslint-disable-next-line max-len
     'M32.46,49.85,58.52,16a2.58,2.58,0,0,0,0-3,1.38,1.38,0,0,0-2.31,0L29,48.35a2.58,2.58,0,0,0,0,3L56.21,86.72a1.36,1.36,0,0,0,2.3,0,2.58,2.58,0,0,0,0-3Z',
@@ -10,18 +10,17 @@ const generellSliderSettings = {
   wrapAround: true,
 };
 
-const pageHeaderSliders = document.querySelectorAll('.ph-slider');
-pageHeaderSliders.forEach((slide) => {
-  new Flickity(slide, {
-    ...generellSliderSettings,
-    autoPlay: 6000,
+const createSlider = (sliders, additionalSettings = {}) => {
+  sliders.forEach((slider) => {
+    new Flickity(slider, {
+      ...generalSliderSettings,
+      ...additionalSettings,
+    });
   });
-});
+};
+
+const pageHeaderSliders = document.querySelectorAll('.ph-slider');
+createSlider(pageHeaderSliders, { autoPlay: 6000 });
 
 const pageBuilderSliders = document.querySelectorAll('.pb-carousel');
-pageBuilderSliders.forEach((slide) => {
-  new Flickity(slide, {
-    ...generellSliderSettings,
-    autoPlay: 8000,
-  });
-});
+createSlider(pageBuilderSliders, { autoPlay: 8000 });
